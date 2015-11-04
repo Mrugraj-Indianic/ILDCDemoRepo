8ff542b7a2a9ab3a5fbb7d0c94ea60b4fe32f170
Mrugrajsinh Vansadia <mrugrajsinh.vansadia@indianic.com>
2015-11-04 23:38:23 +0530

    Initial Commit and setup

---
+++ ILDCGitDemo/ILDCGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ILDCGitDemo
+//
+//  Created by indianic1 on 04/11/15.
+//  Copyright © 2015 IndiaNIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ILDCGitDemo/ILDCGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ILDCGitDemo
+//
+//  Created by indianic1 on 04/11/15.
+//  Copyright © 2015 IndiaNIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
